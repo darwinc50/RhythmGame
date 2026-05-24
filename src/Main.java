222bc0ff4130e3b8b47cf1b3dcb0a2178772c70a
d <clxmr3@gmail.com>
2026-05-23 20:54:02 -0400

    i did some funny stuff

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,15 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("sick pain in the ass game that edward wanted to make we are deadbutt so screwed lol");
-        frame.setIconImage(new ImageIcon("src/astolfo.jpg").getImage());
+        frame.setIconImage(new ImageIcon("src/spinnin.png").getImage()); //change app icon image
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1920, 1080);
         frame.setLocationRelativeTo(null);
         PlaySong dangerousWoman = new PlaySong(new File("src/songs/dangerouswoman.wav"));
         PlaySong takeAHint = new PlaySong(new File("src/songs/takeahint.wav"));
+        PlaySong iminlove = new PlaySong(new File("src/songs/iminlove.wav"));
         // create a DisplayPanel object
-        DisplayWindow panel = new DisplayWindow(takeAHint,frame);
+        DisplayWindow panel = new DisplayWindow(iminlove,frame);
 
         // add it to the frame
         frame.add(panel);
